aa13e0072be24c9bf1d36f1000cf39d3042a0d81
rafayoko <r205072@dac.unicamp.br>
2021-06-12 14:07:50 -0300

    cód

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-let url = "https://tt905-2021-progweb-mensagens.herokuapp.com/mensagens"
+let url = "https://tt905-2021-progweb-mensagens.herokuapp.com/mensagens/"
 
 async function callFetchWithGet(){
     let headers = new Headers();
@@ -21,7 +21,7 @@ async function callFetchWithGet(){
 }
 
 async function callFetchWithPost(mensagem){
-    let headers = new Headers();
+
     const options = {
         method : 'Post',
         mode: 'cors',
@@ -32,21 +32,68 @@ async function callFetchWithPost(mensagem){
         body: JSON.stringify({
             'mensagem':mensagem
         })
-    } await fetch(url,options);
+    } 
+    await fetch(url,options);
 }
 
-async function callFetchWithPut(){
-    let headers = new Headers();
+async function callFetchWithPut(id, novaMensagem){
     const options = {
         method : 'Put',
         mode: 'cors',
-        headers: headers 
-    }
-
-async function callFetchWithDelete(){
-    let headers = new Headers();
+        headers: {
+            'Accept':'application/json',
+            'content-type' : 'application/json'
+        },
+        body: JSON.stringify({
+            'mensagem': novaMensagem
+        })
+    }   
+  await fetch(`${url}${id}`, options);
+}
+  
+async function callFetchWithDelete(id){
     const options = {
         method : 'Delete',
         mode: 'cors',
-        headers: headers 
-    }
+        headers: {
+            'Accept':'application/json',
+            'content-type' : 'application/json'
+        }
+    }   
+  await fetch(`${url}${id}`, options);
+}
+  
+
+//Formulários
+
+function submitPost(){
+    console.log("entrei na função");
+    const form = document.forms['postForm'];
+    const mensagem = form["mensagem"].value;
+    callFetchWithPost(mensagem);
+    return false; //evitar o reload na tela.
+ 
+}
+
+//Formulários
+
+function submitPut(){
+
+    const form = document.forms['putForm'];
+    const id = form["id"].value;
+    const mensagem = form["mensagem"].value;
+    callFetchWithPut(id,mensagem);
+    return false; //evitar o reload na tela.
+ 
+}
+
+
+function submitDelete(){
+
+    const form = document.forms['deleteForm'];
+    const id = form["id"].value;
+
+    callFetchWithDelete(id );
+    return false; //evitar o reload na tela.
+ 
+}
